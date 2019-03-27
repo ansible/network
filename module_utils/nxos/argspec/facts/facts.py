cfc870c5a0412326a24a0d87729e9268c027dc47
Trishna Guha <trishnaguha17@gmail.com>
2019-03-27 12:31:13 +0530

    Address review comment
    Signed-off-by: Trishna Guha <trishnaguha17@gmail.com>

--- module_utils/nxos/argspec/facts/facts.py
+++ module_utils/nxos/argspec/facts/facts.py
@@ -2,6 +2,11 @@ from ansible.module_utils.network.argspec.base import ArgspecBase
 
 class FactsArgs(ArgspecBase):
 
+    choices = [
+        'all',
+        'net_configuration_interfaces',
+    ]
+
     argument_spec = {
-        'gather_subset': dict(default=['all'], type='list')
+        'gather_subset': dict(default=['all'], choices=choices, type='list')
     }
--- module_utils/nxos/facts/facts.py
+++ module_utils/nxos/facts/facts.py
@@ -6,28 +6,28 @@ from ansible.module_utils.nxos.facts.interfaces.interfaces import InterfacesFact
 
 class Facts(FactsArgs, FactsBase):
 
-    VALID_SUBSETS = [
-        'net_configuration_interfaces',
-    ]
-
     def get_facts(self, module, connection, gather_subset=['all']):
+        valid_subsets = self.argument_spec['gather_subset'].get('choices', [])
+        if valid_subsets and 'all' in valid_subsets:
+            valid_subsets.remove('all')
+
         runable_subsets = set()
         exclude_subsets = set()
 
         for subset in gather_subset:
             if subset == 'all':
-                runable_subsets.update(self.VALID_SUBSETS)
+                runable_subsets.update(valid_subsets)
                 continue
             if subset.startswith('!'):
                 subset = subset[1:]
                 if subset == 'all':
-                    exclude_subsets.update(self.VALID_SUBSETS)
+                    exclude_subsets.update(valid_subsets)
                     continue
                 exclude = True
             else:
                 exclude = False
 
-            if subset not in self.VALID_SUBSETS:
+            if subset not in valid_subsets:
                 module.fail_json(msg='Bad subset')
 
             if exclude:
@@ -36,7 +36,7 @@ class Facts(FactsArgs, FactsBase):
                 runable_subsets.add(subset)
 
         if not runable_subsets:
-            runable_subsets.update(self.VALID_SUBSETS)
+            runable_subsets.update(valid_subsets)
 
         runable_subsets.difference_update(exclude_subsets)
         self.ansible_facts['gather_subset'] = list(runable_subsets)
