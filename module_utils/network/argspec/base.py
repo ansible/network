6f905737db240e7d8eaf69bee16c3cd50e33d5bf
Sumit Jaiswal <sjaiswal@redhat.com>
2019-04-09 22:40:32 +0530

    ios interface initial commit
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>

---
+++ module_utils/network/argspec/base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2019 <company_name>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+""" The argspec base class
+"""
+
+class ArgspecBase(object): #pylint: disable=R0205,R0903
+    """ The argspec base class
+    """
+    def __init__(self, **kwargs):
+        pass
