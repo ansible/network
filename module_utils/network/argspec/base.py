3b6952b05669ba83ffc49a09ce20a5294dd37e7c
Sumit Jaiswal <sjaiswal@redhat.com>
2019-04-12 16:33:53 +0530

    iosxr interface initial commit
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>

---
+++ module_utils/network/argspec/base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2019 Red Hat Inc.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+""" The argspec base class
+"""
+
+class ArgspecBase(object):
+    """ The argspec base class
+    """
+    def __init__(self, **kwargs):
+        pass
