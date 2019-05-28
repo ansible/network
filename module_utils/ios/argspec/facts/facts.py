0c355e3d508686a778ba347cfb1f40d953b117c5
Sumit Jaiswal <sjaiswal@redhat.com>
2019-05-28 12:46:08 +0530

    cosmetic change
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>

--- module_utils/ios/argspec/facts/facts.py
+++ module_utils/ios/argspec/facts/facts.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# {{ rm['COPYRIGHT'] }}
+# Copyright 2019 Red Hat Inc.
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 """
-The arg spec for the iosxr facts module.
+The arg spec for the ios facts module.
 """
 
 class FactsArgs(object): #pylint: disable=R0903
