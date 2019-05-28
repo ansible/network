0c6250fc8681fa3e27162a3cbe375d5eef469a5f
Sumit Jaiswal <sjaiswal@redhat.com>
2019-05-28 12:41:00 +0530

    adhered to new facts
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>

--- module_utils/ios/argspec/facts/facts.py
+++ module_utils/ios/argspec/facts/facts.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright 2019 Red Hat Inc.
+# {{ rm['COPYRIGHT'] }}
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 """
-The arg spec for the ios facts module.
+The arg spec for the iosxr facts module.
 """
 
-class FactsArgs(object):
+class FactsArgs(object): #pylint: disable=R0903
+    """ The arg spec for the ios facts module
+    """
     def __init__(self, **kwargs):
         pass
 
     choices = [
         'all',
-        'net_configuration_interfaces',
+        'interfaces',
     ]
-
     argument_spec = {
-        'gather_subset': dict(default=['all'], choices=choices, type='list')
+        'gather_subset': dict(default=['!config'], type='list'),
+        'gather_network_resources': dict(default=['all'], choices=choices, type='list'),
     }
-
