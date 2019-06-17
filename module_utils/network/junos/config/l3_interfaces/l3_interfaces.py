da6e00daa86f723eb72cec6831d36563c4e0897e
Daniel Mellado <dmellado@redhat.com>
2019-06-17 10:21:49 +0200

    [WIP] Add junos_l3_interfaces resource module

---
+++ module_utils/network/junos/config/l3_interfaces/l3_interfaces.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2019 Red Hat
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""
+The junos_l3_interfaces class
+It is in this file where the current configuration (as dict)
+is compared to the provided configuration (as dict) and the command set
+necessary to bring the current configuration to it's desired end-state is
+created
+"""
+
+from ansible.module_utils.network.common.utils import to_list
+
+from ansible.module_utils.network. \
+    junos.argspec.l3_interfaces.l3_interfaces import L3_interfacesArgs
+from ansible.module_utils.network. \
+    junos. \
+    config.base import ConfigBase
+from ansible.module_utils.network. \
+    junos.facts.facts import Facts
+
+
+class L3_interfaces(ConfigBase, L3_interfacesArgs):
+    """
+    The junos_l3_interfaces class
+    """
+
+    gather_subset = [
+        '!all',
+        '!min',
+    ]
+
+    gather_network_resources = [
+        'l3_interfaces',
+    ]
+
+    def get_l3_interfaces_facts(self):
+        """ Get the 'facts' (the current configuration)
+
+        :rtype: A dictionary
+        :returns: The current configuration as a dictionary
+        """
+        facts, _warnings = Facts().get_facts(self._module,
+                                             self._connection,
+                                             self.gather_subset,
+                                             self.gather_network_resources)
+        anr_facts = facts['ansible_network_resources']
+        l3_interfaces_facts = anr_facts.get('l3_interfaces')
+        if not l3_interfaces_facts:
+            return []
+        return l3_interfaces_facts
+
+    def execute_module(self):
+        """ Execute the module
+
+        :rtype: A dictionary
+        :returns: The result from module execution
+        """
+        result = {'changed': False}
+        commands = list()
+        warnings = list()
+
+        existing_l3_interfaces_facts = self.get_l3_interfaces_facts()
+        commands.extend(self.set_config(existing_l3_interfaces_facts))
+        if commands:
+            if not self._module.check_mode:
+                self._connection.edit_config(commands)
+            result['changed'] = True
+        result['commands'] = commands
+
+        changed_l3_interfaces_facts = self.get_l3_interfaces_facts()
+
+        result['before'] = existing_l3_interfaces_facts
+        if result['changed']:
+            result['after'] = changed_l3_interfaces_facts
+
+        result['warnings'] = warnings
+        return result
+
+    def set_config(self, existing_l3_interfaces_facts):
+        """ Collect the configuration from the args passed to the module,
+            collect the current configuration (as a dict from facts)
+
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        want = self._module.params['config']
+        have = existing_l3_interfaces_facts
+        resp = self.set_state(want, have)
+        return to_list(resp)
+
+    def set_state(self, want, have):
+        """ Select the appropriate function based on the state provided
+
+        :param want: the desired configuration as a dictionary
+        :param have: the current configuration as a dictionary
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        state = self._module.params['state']
+        if state == 'overridden':
+            kwargs = {}
+            commands = self._state_overridden(**kwargs)
+        elif state == 'deleted':
+            kwargs = {}
+            commands = self._state_deleted(**kwargs)
+        elif state == 'merged':
+            kwargs = {}
+            commands = self._state_merged(**kwargs)
+        elif state == 'replaced':
+            kwargs = {}
+            commands = self._state_replaced(**kwargs)
+        return commands
+
+    @staticmethod
+    def _state_replaced(**kwargs):
+        """ The command generator when state is replaced
+
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        commands = []
+        return commands
+
+    @staticmethod
+    def _state_overridden(**kwargs):
+        """ The command generator when state is overridden
+
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        commands = []
+        return commands
+
+    @staticmethod
+    def _state_merged(**kwargs):
+        """ The command generator when state is merged
+
+        :rtype: A list
+        :returns: the commands necessary to merge the provided into
+                  the current configuration
+        """
+        commands = []
+        return commands
+
+    @staticmethod
+    def _state_deleted(**kwargs):
+        """ The command generator when state is deleted
+
+        :rtype: A list
+        :returns: the commands necessary to remove the current configuration
+                  of the provided objects
+        """
+        commands = []
+        return commands
